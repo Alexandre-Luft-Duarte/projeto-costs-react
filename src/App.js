e611c26d9a72a7ea6cb727fd5cfd1601a4d1ace8
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-05-16 12:00:49 -0300

    navbar, footer, css criados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Company from './pages/Company';
+import NewProject from './pages/NewProject';
+import Container from './layout/Container';
+import NavBar from './layout/Navbar';
+import Projects from './pages/Projects';
+import Footer from './layout/Footer';
+
+
 function App() {
   return (
-    <div>
-        <p>Costs</p>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Container customClass="min-height" ><Home /></Container>} />
+        <Route path="/contact" element={<Container customClass="min-height" ><Contact /></Container>} />
+        <Route path="/company" element={<Container customClass="min-height" ><Company /></Container>} />
+        <Route path="/newproject" element={<Container customClass="min-height" ><NewProject /></Container>} />
+        <Route path="/projects" element={<Container customClass="min-height" ><Projects /></Container>} />
+      </Routes>
+      <Footer /> 
+    </Router>
   );
 }
 
