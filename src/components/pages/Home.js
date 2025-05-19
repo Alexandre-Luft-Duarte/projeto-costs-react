10ee39b9d9e8bacac872675db9b90f9d47f322fe
Alexandre-Luft-Duarte <alexandreduarte43234@gmail.com>
2025-05-19 14:58:54 -0300

    restauração das pastas e arquivos excluidos sem querer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home(){
         <section className={styles.home_container}> 
             <h1>Bem-vindo ao Cost</h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <LinkButton to="/newproject" text="Criar Projeto"/> /* LinkButton é um componente que renderiza um botão que redireciona para a página de criação de projeto */
+            <LinkButton to="/newproject" text="Criar Projeto"/> {/* LinkButton é um componente que renderiza um botão que redireciona para a página de criação de projeto */}
             <img src={savings} alt='Costs'/>
         </section>
     );
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,34 +1,36 @@
 import styles from './ProjectForm.module.css';
-import Form from '../form/components/Form';
-import Select from '../form/components/Select';
-import SubmitButton from '../form/components/SubmitButton';
-import {useState, useEffect} from 'react'
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import {useEffect, useState} from 'react'
 
 function ProjectForm ({btnText}){
-    
     const[categories, setCategories] = useState([])
-
+    
     useEffect(() => {
-        fetch('http://localhost:5000/categories', { /*faz uma requisição para a API local*/
-            method: 'GET', /* método GET para buscar os dados*/
-            headers: { /* define o tipo de conteúdo da requisição*/
-                'Content-Type': 'application/json'
-            },
-        })
-            .then((resp) => resp.json()) /* Quando a resposta chega, ela é convertida de JSON para um objeto JavaScript.*/
-            .then((data) => setCategories(data)) /* O resultado é armazenado no estado categories usando o setCategories. */
-            .catch((err) => console.log(err)); /* Se houver um erro, ele é capturado e exibido no console. */
-    }, []); // <- esse array vazio faz rodar só uma vez
+    fetch('http://localhost:5000/categories', { /*faz uma requisição para a API local*/
+        method: 'GET', /* método GET para buscar os dados*/
+        headers: { /* define o tipo de conteúdo da requisição*/
+            'Content-Type': 'application/json'
+        },
+    })
+        .then((resp) => resp.json()) /* Quando a resposta chega, ela é convertida de JSON para um objeto JavaScript.*/
+        .then((data) => {
+            setCategories(data)
+        }) /* O resultado é armazenado no estado categories usando o setCategories. */
+        .catch((err) => console.log(err)); /* Se houver um erro, ele é capturado e exibido no console. */
+    }, [])
+
 
     return(
         <form className={styles.form}>
-            <Form 
+            <Input 
                 type="text" 
                 text="Nome do Projeto" 
                 name="name" 
                 placeholder="Insira o Projeto"
             />    
-            <Form 
+            <Input 
                 type="number" 
                 text="Valor do Orçamento" 
                 name="budget" 
